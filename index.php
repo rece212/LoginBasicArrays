a2e214b512ccc415a0f6fe5e689b2b94590b14a7
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-10 15:54:07 +0200

    added MD5 hash to the program

--- index.php
+++ index.php
@@ -1,10 +1,19 @@
 <?php
     session_start();
 
-    $Username = Array("Granny","Grandpa","Grandson");
-    $Password= Array("Cats","Ball","Dog");
+    $Username = Array("782dd5249e6dfe9a887c44e4370b7564","16ba038ec0ed9f6326e73bb398b3bf29","c2f970ce4f3884dd997e915bc9f11efc");
+    $Password= Array("6839d672141795d0959700017e3cdec4","353df421c4fc976e2637061d7a83f601","c935d187f0b998ef720390f85014ed1e");
+
     $Name = isset($_POST["username"])?$_POST["username"] :'';
     $Pass = isset($_POST["password"])?$_POST["password"] :'';
+    $ErrorMessage="";// this is for error that accuse
+
+    if (isset($_POST['login']))
+    {
+        Login($Name,$Pass,$Username,$Password);
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -35,6 +44,7 @@
 
     <label>Password</label>
     <input type="password" required name="password"  value="<?php  echo isset($Pass) ? $Pass:'';?>" placeholder="password"><br>
+    <?php echo $ErrorMessage;?>
 
     <!--Login Button-->
     <button name="login" type="Submit">Login</button>
@@ -47,32 +57,29 @@
 
 
 <?php
-if (isset($_POST['login']))
-{
-    Login($Name,$Pass,$Username,$Password);
-}
 
-
-function Login($Name,$Pass,$Username,$Password)
-{
-    $Logged =false;
-    for ( $x=0;$x<3;$x++)
+    function Login($Name,$Pass,$Username,$Password)
     {
-        if((strcmp($Username[$x],$Name)==0)&& (strcmp($Password[$x],$Pass)==0))
+        $Logged =false;
+        for ( $x=0;$x<3;$x++)
         {
-            echo '<script>alert("You got your details correct")</script>';
-            $Logged=true;
-            $_SESSION['name'] = $Name;
-            header('Location: Welcome.php');
+            if((strcmp($Username[$x],md5($Name))==0)&& (strcmp($Password[$x],md5($Pass))==0))
+            {
+                echo '<script>alert("You got your details correct")</script>';
+                $Logged=true;
+                $_SESSION['name'] = $Name;
+                header('Location: Welcome.php');
 
+            }
         }
-    }
-    if ($Logged ==false)
-    {
-        echo '<script>alert("You got your details wrong")</script>';
+        if ($Logged ==false)
+        {
+            //echo '<script>alert("You got your details wrong")</script>';
+            $GLOBALS['ErrorMessage']="<br>You got your details wrong<br>";
+        }
 
     }
 
-}
+
 
 ?>
