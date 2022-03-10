d6245ab775f4b8d38cb87f7f0a9934a82e4dfec2
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-10 16:09:03 +0200

    Added home work in the comments

--- index.php
+++ index.php
@@ -1,6 +1,9 @@
 <?php
     session_start();
-
+    //  home work - user must be able to also register an account
+    // be able to login with the new user details
+    // must create a reg screen ( with css)
+   // make a nice and fun welcome screen
     $Username = Array("782dd5249e6dfe9a887c44e4370b7564","16ba038ec0ed9f6326e73bb398b3bf29","c2f970ce4f3884dd997e915bc9f11efc");
     $Password= Array("6839d672141795d0959700017e3cdec4","353df421c4fc976e2637061d7a83f601","c935d187f0b998ef720390f85014ed1e");
 
