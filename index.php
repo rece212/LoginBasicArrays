29828638aabcb6d6d0c9c27392a761492de986c9
Reece Wanvig <rwanvig@iie.ac.za>
2022-03-10 16:07:04 +0200

    added MD5 hash to the program

--- index.php
+++ index.php
@@ -4,13 +4,25 @@
     $Username = Array("782dd5249e6dfe9a887c44e4370b7564","16ba038ec0ed9f6326e73bb398b3bf29","c2f970ce4f3884dd997e915bc9f11efc");
     $Password= Array("6839d672141795d0959700017e3cdec4","353df421c4fc976e2637061d7a83f601","c935d187f0b998ef720390f85014ed1e");
 
+    $Data = Array(  Array("782dd5249e6dfe9a887c44e4370b7564","6839d672141795d0959700017e3cdec4"),
+                    Array("16ba038ec0ed9f6326e73bb398b3bf29","353df421c4fc976e2637061d7a83f601"),
+                    Array("c2f970ce4f3884dd997e915bc9f11efc","c935d187f0b998ef720390f85014ed1e"));
+
+
+    $multLogin =array(
+            Array("Granny","Cats"),
+            Array("Grandpa","Ball"),
+            Array("Grandson","Dog")
+        );
+
+
     $Name = isset($_POST["username"])?$_POST["username"] :'';
     $Pass = isset($_POST["password"])?$_POST["password"] :'';
     $ErrorMessage="";// this is for error that accuse
 
     if (isset($_POST['login']))
     {
-        Login($Name,$Pass,$Username,$Password);
+        Login($Name,$Pass,$Data);
     }
 
 
@@ -58,12 +70,12 @@
 
 <?php
 
-    function Login($Name,$Pass,$Username,$Password)
+    function Login($Name,$Pass,$Date)
     {
         $Logged =false;
         for ( $x=0;$x<3;$x++)
         {
-            if((strcmp($Username[$x],md5($Name))==0)&& (strcmp($Password[$x],md5($Pass))==0))
+            if((strcmp($Date[$x][0],md5($Name))==0)&& (strcmp($Date[$x][1],md5($Pass))==0))
             {
                 echo '<script>alert("You got your details correct")</script>';
                 $Logged=true;
